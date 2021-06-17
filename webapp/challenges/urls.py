e48c5ae3b8cae11b6d42fdc1b8c2e0073a41f856
Prasetya Adi <utopia.adi@gmail.com>
2021-06-18 06:50:28 +0700

    Menambah variabel pada URL dengan simple, cukup <month> dan selesai

--- webapp/challenges/urls.py
+++ webapp/challenges/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from django.urls import path
+from django.urls import path, re_path
 
 from . import views
 
 urlpatterns = [
-    path("Januari", views.Januari),
-    path("Februari", views.Februari),
+    re_path(r'^(?P<bulan>[\w-]+)/$', views.Random),
+    path('v2/<bulan2>', views.Random2),
+    path('v2/<int:bulan2>', views.Random3),
 ]
--- webapp/challenges/views.py
+++ webapp/challenges/views.py
@@ -4,9 +4,32 @@ from django.http import HttpResponse
 # Create your views here.
 
 
-def Januari(request):
-    return HttpResponse("ini Januari")
+def Random(request, bulan):
+    Heading = "Ini Bulan dengan REGEX. Bulan ini adalah: "
+    str = Heading + bulan
+
+    return HttpResponse(str)
 
 
-def Februari(request):
-    return HttpResponse("ini Februari")
+def Random2(request, bulan2):
+    Heading = "Ini Bulan dengan IF. Bulan ini adalah: "
+    if bulan2 == "Januari":
+        bulan = "Januari"
+        str = Heading + bulan
+    else:
+        lain = "ini bulan lainnya"
+        str = Heading + lain
+
+    return HttpResponse(str)
+
+
+def Random3(request, bulan2):
+    Heading = "Ini Bulan dengan IF. Bulan ini adalah: "
+    if bulan2 == "Januari":
+        bulan = "Januari"
+        str = Heading + bulan
+    else:
+        lain = "ini bulan lainnya"
+        str = Heading + lain
+
+    return HttpResponse(str)
