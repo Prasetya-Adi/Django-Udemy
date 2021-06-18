a5ea2f75f18fb7a04b911260e612f5ec60e45536
Prasetya Adi <utopia.adi@gmail.com>
2021-06-18 10:44:16 +0700

    finish tutorial URL, done membuat list link untuk semua url

--- webapp/challenges/views.py
+++ webapp/challenges/views.py
@@ -1,9 +1,29 @@
+from django.http import response
 from django.http.response import HttpResponseNotFound
 from django.shortcuts import redirect, render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 
 # Create your views here.
+
+Bulan = {
+    "Januari": "Ini LIST bulan Januari",
+    "Februari": "Ini LIST bulan Februari",
+    "Maret": "Ini LIST bulan Maret",
+    "April": "Ini LIST bulan April",
+}
+
+
+def index(request):
+    list_bulan = ""
+    nama_bulan = list(Bulan.keys())
+    for bulan in nama_bulan:
+
+        urlBulan = reverse('bulanreverse', args=[bulan])
+        list_bulan += f'<li><a href=\"{urlBulan}\">{bulan}</a></li>'
+
+    responeData = f"<ul>{list_bulan}</ul>"
+    return HttpResponse(responeData)
 
 
 def Random1(request, bulan):
@@ -25,14 +45,6 @@ def Random2(request, bulan):
     return HttpResponse(str)
 
 
-Bulan = {
-    "Januari": "Ini LIST bulan Januari",
-    "Februari": "Ini LIST bulan Februari",
-    "Maret": "Ini LIST bulan Maret",
-    "April": "Ini LIST bulan April",
-}
-
-
 def Random3(request, bulan):
     try:
         str = Bulan[bulan]
@@ -51,6 +63,3 @@ def Random5(request, bulan):
     bulankeys = list(Bulan.keys())
     redirectbulan = bulankeys[bulan-1]
     return HttpResponseRedirect(reverse('bulanreverse', args=[redirectbulan]))
-
-
-# mencoba gith2
