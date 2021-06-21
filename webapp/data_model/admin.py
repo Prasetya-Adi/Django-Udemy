c322bf83d3d61b48bdbcb255fdd4a9c0829a5001
Prasetya Adi <utopia.adi@gmail.com>
2021-06-21 16:32:54 +0700

    finish django admin di udemy

--- webapp/data_model/admin.py
+++ webapp/data_model/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from .models import Book
+
 # Register your models here.
+
+
+class BookAdmin(admin.ModelAdmin):
+    readonly_fields = ['slug']
+
+
+admin.site.register(Book, BookAdmin)
--- webapp/data_model/models.py
+++ webapp/data_model/models.py
@@ -12,7 +12,8 @@ class Book(models.Model):
         validators=[MinValueValidator(1), MaxValueValidator(5)])
     author = models.CharField(null=True, max_length=100)
     is_bestselling = models.BooleanField(default=False)
-    slug = models.SlugField(default="", null=False, db_index=True)
+    slug = models.SlugField(default="", blank=True,
+                            null=False, editable=False, db_index=True)
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.title)
