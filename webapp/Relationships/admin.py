1c7ff935e31b79077c77a077d47b444953150fd3
Prasetya Adi <utopia.adi@gmail.com>
2021-06-22 21:09:40 +0700

    finish relationships

--- webapp/Relationships/admin.py
+++ webapp/Relationships/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
-from .models import Book
+from .models import Address, Author, Book, Country
 # Register your models here.
 
-admin.site.register(Book)
+
+class BookAdmin(admin.ModelAdmin):
+    # readonly_fields = ('slug',)
+    prepopulated_fields = {'slug': ('title',), }
+    list_filter = ('rating', 'is_bestselling')
+    list_display = ('title', 'author')
+
+
+admin.site.register(Book, BookAdmin)
+admin.site.register(Author)
+admin.site.register(Address)
+admin.site.register(Country)
--- webapp/Relationships/models.py
+++ webapp/Relationships/models.py
@@ -1,22 +1,64 @@
 from django.db import models
 from django.db.models.base import Model
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django.db.models.deletion import CASCADE
 from django.utils.text import slugify
 # Create your models here.
+
+
+class Country(models.Model):
+    name = models.CharField(max_length=80)
+    code = models.CharField(max_length=2)
+
+    def __str__(self):
+        return f'{self.name}, {self.code}'
+
+    class Meta:
+        verbose_name_plural = "All Country"
+
+
+class Address(models.Model):
+    street = models.CharField(max_length=80)
+    postal_code = models.CharField(max_length=5)
+    city = models.CharField(max_length=50)
+
+    def fullAddress(self):
+        return f'{self.street} {self.city}, {self.postal_code}'
+
+    def __str__(self):
+        return self.fullAddress()
+
+    class Meta:
+        verbose_name_plural = "Address Entries"
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    address = models.OneToOneField(Address, on_delete=CASCADE, null=True)
+
+    def fullname(self):
+        return f'{self.first_name} {self.last_name}'
+
+    def __str__(self):
+        return self.fullname()
 
 
 class Book(models.Model):
     title = models.CharField(max_length=50)
     rating = models.IntegerField(
         validators=[MinValueValidator(1), MaxValueValidator(5)])
-    author = models.CharField(null=True, max_length=100)
+    author = models.ForeignKey(
+        Author, on_delete=models.CASCADE, null=True, related_name='books')
     is_bestselling = models.BooleanField(default=False)
     slug = models.SlugField(default="", blank=True,
-                            null=False, editable=True, db_index=True)
+                            null=False, db_index=True)
+    published_country = models.ManyToManyField(Country)
 
-    def save(self, *args, **kwargs):
-        self.slug = slugify(self.title)
-        super().save(*args, **kwargs)
+    # Sudah menggunakan prepopulated_field di admin
+    # def save(self, *args, **kwargs):
+    #     self.slug = slugify(self.title)
+    #     super().save(*args, **kwargs)
 
     def __str__(self):
         return f'title: {self.title}, author: {self.author}'
