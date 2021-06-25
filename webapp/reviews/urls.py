c6b3ec30135ed5ecf6a5a168c30ba5e9caf3dcd8
Prasetya Adi <utopia.adi@gmail.com>
2021-06-25 15:47:12 +0700

    Menambah form dan validasi form menggunakan class Bootstrap

--- webapp/reviews/urls.py
+++ webapp/reviews/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("you", views.you, name='youReviews'),
     path("", views.index, name='HomeReviews'),
 ]
--- webapp/reviews/views.py
+++ webapp/reviews/views.py
@@ -1,12 +1,34 @@
+from django.conf.urls import url
 from django.http.response import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
 
 # Create your views here.
 
 
 def index(request):
     contex = {
-        'title': 'Reviews'
-        'content': 'content'
+        'title': 'Reviews',
+        'has_error': False,
     }
+
+    if request.method == "POST":
+        entered_username = request.POST['username']
+        entered_review = request.POST['review']
+
+        if entered_username == '':
+            return render(request, 'reviews/index.html',  {
+                'has_error': True,
+            })
+
+        print(entered_username)
+        print(entered_review)
+        return HttpResponseRedirect('/reviews/you')
+
     return render(request, 'reviews/index.html', contex)
+
+
+def you(request):
+    contex = {
+        'title': 'Thank You',
+    }
+    return render(request, 'reviews/thankyou.html', contex)
