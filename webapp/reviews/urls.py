5472dc36de9b1ba8fce63bb006dbb6c77e75105a
Prasetya Adi <utopia.adi@gmail.com>
2021-06-26 23:38:51 +0700

    finish Class View, update all the views into Class

--- webapp/reviews/urls.py
+++ webapp/reviews/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("list/<int:pk>", views.SingleReviewView.as_view(), name='one_Reviews'),
+    path("list", views.ReviewListView.as_view(), name='listReviews'),
     path("you", views.thankyouView.as_view(), name='youReviews'),
     path("", views.ReviewView.as_view(), name='HomeReviews'),
 ]
--- webapp/reviews/views.py
+++ webapp/reviews/views.py
@@ -1,26 +1,51 @@
+from django.db.models.base import Model
+from django.forms.forms import Form
+import reviews
 from django.conf.urls import url
 from django.http.response import HttpResponse
 from django.shortcuts import render, HttpResponseRedirect
 from django.views import View
 from django.views.generic.base import TemplateView
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import FormView, CreateView, UpdateView, DeleteView
+
 
 from .forms import ReviewForms
+from .models import Review
 
 # Create your views here.
 
+# FIRST OPTION
+# class ReviewView(View):
+#     def get(self, request):
+#         form = ReviewForms()
+#         return render(request, 'reviews/index.html', {'title': "Reviews", 'form': form})
 
-class ReviewView(View):
-    def get(self, request):
-        form = ReviewForms()
-        return render(request, 'reviews/index.html', {'title': "Reviews", 'form': form})
+#     def post(self, request):
+#         form = ReviewForms(request.POST)
 
-    def post(self, request):
-        form = ReviewForms(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return HttpResponseRedirect('/reviews/you')
+#         return render(request, 'reviews/index.html', {'title': "Reviews", 'form': form})
 
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect('/reviews/you')
-        return render(request, 'reviews/index.html', {'title': "Reviews", 'form': form})
+
+# SECOND OPTION
+# class ReviewView(FormView):
+#     form_class = ReviewForms
+#     template_name = 'reviews/index.html'
+#     success_url = '/reviews/you'
+
+#     def form_valid(self, form):
+#         form.save()
+#         return super().form_valid(form)
+
+# THIRD OPTION
+class ReviewView(CreateView):
+    Model = Review
+    form_class = ReviewForms
+    template_name = 'reviews/index.html'
+    success_url = '/reviews/you'
 
 
 class thankyouView(TemplateView):
@@ -30,3 +55,41 @@ class thankyouView(TemplateView):
         contex = super().get_context_data(**kwargs)
         contex['title'] = 'Thank You'
         return contex
+
+
+# class ReviewListView(TemplateView):
+#     template_name = 'reviews/review_list.html'
+
+#     def get_context_data(self, **kwargs):
+#         review = Review.objects.all()
+#         contex = super().get_context_data(**kwargs)
+#         contex['form'] = review
+#         return contex
+
+
+# ListView for show the list of database in an easy way
+class ReviewListView(ListView):
+    template_name = 'reviews/review_list.html'
+    model = Review
+    context_object_name = 'reviews'
+
+    # query for getting more than 4
+    def get_queryset(self):
+        basequery = super().get_queryset()
+        data = basequery.filter(rating__gt=4)
+        return data
+
+
+# class SingleReviewView(TemplateView):
+#     template_name = 'reviews/single_review.html'
+
+#     def get_context_data(self, **kwargs):
+#         review_id = kwargs['id']
+#         review = Review.objects.get(pk=review_id)
+#         contex = super().get_context_data(**kwargs)
+#         contex['review'] = review
+#         return contex
+
+class SingleReviewView(DetailView):
+    template_name = 'reviews/single_review.html'
+    model = Review
